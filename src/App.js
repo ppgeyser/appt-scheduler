b88fa732f219b5ee76e37d04472b6d156439dadf
Peter Geyser <peterpgeyser@gmail.com>
2020-01-14 13:11:34 -0800

    initial files from offline work

---
+++ src/App.js
@@ -0,0 +1,35 @@
+//Dependencies
+import React from "react";
+import store from "store";
+import {
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+//Components
+import AppJumbotron from "./components/AppJumbotron";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <Container fluid>
+
+      <AppJumbotron />
+
+      <Row>
+
+        <Col sm="3">
+          <InputForm />
+        </Col>
+
+        <Col sm="9">
+        </Col>
+
+      </Row>
+
+    </Container>
+  );
+}
+
+export default App;
